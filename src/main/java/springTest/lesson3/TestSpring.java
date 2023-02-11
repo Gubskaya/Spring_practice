9e9cb3c3cc031bd4548c914456b77d30a6a4792c
daria <gybskaya.dd@gmail.com>
2023-02-11 22:40:01 +0600

    commit

--- src/main/java/springTest/lesson3/TestSpring.java
+++ src/main/java/springTest/lesson3/TestSpring.java
@@ -7,9 +7,17 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
+        //внедрение зависимости вручную
         Music music = context.getBean("musicBean", Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(music); //IoC
         musicPlayer.playMusic();
+
+        //внедрение зависимости при помощи Spring'a
+        //Dependency injection
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class); //в параметр передается бин musicPlayer, которому в конструктор передали бин musicBean
+        musicPlayer2.playMusic();
+
         context.close();
     }
 }
